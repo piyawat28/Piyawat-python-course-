a1b40a914bff6ba9603b416781a7fd235063b2f0
piyawat28 <piyawat.pai@ku.th>
2025-09-11 15:13:44 +0700

    Piyawat

--- assignments/week08/quiz-inheritance.py
+++ assignments/week08/quiz-inheritance.py
@@ -6,3 +6,18 @@ Create a class hierarchy:
     Implement a method get_info() in both classes
 
 """
+class Vehicle:
+    def __init__(self,brand,model,year):
+        self.brand = brand
+        self.model = model
+        self.year  = year
+class car(Vehicle): #คลาส car รับถ่ายทอดจากคลาส Vehicle
+    def __init__(self,brand,model,year,number_of_doors):
+        super().__init__(brand,model,year)
+        self.number_of_doors = number_of_doors
+        
+    def get_info(self):#overriding method
+        return f"brand: {self.brand},Model: {self.model},year:{self.year},number of Doors: {self.number_of_doors}"
+
+mycar = car ("Toyota","Yaris cross",2025,4)
+print(mycar.get_info())
