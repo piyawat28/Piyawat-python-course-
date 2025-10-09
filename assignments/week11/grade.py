fb4f86f175895532ad75f49e6a04dafeceff4271
piyawat28 <piyawat.pai@ku.th>
2025-10-09 14:33:01 +0700

    Piyawat

--- assignments/week11/grade.py
+++ assignments/week11/grade.py
@@ -18,5 +18,40 @@ A global variable passing_grade = 50
 
 - Loops through students
 - Uses the global passing_grade to determine pass/fail
-- Prints each student's name, average, and status (PASS/FAIL)
+- Prints ea,ch student's name, average, and status (PASS/FAIL)
 """
+
+passing_grade = 50
+
+def input_students(num_students):
+    students = [
+        {
+        'name': 'Boonchoo',
+        'scores': [45,52,63]
+    }
+    {
+        'name': 'steve',
+        'scores': [87,75,68]
+}
+    ]
+    return students
+def calculet_average(students):
+    for student in students:
+        sum=0
+        for score in student['scores']:
+            sum = sum+score
+        student['average'] = sum / len(student['scores'])
+
+def display_results(students):
+    for student in student:
+        print("student Name: %s" %(student['Name']))
+        print("student average Score: %s2f" %(student['average']))
+        if student['average'] >= passing_grade:
+            print("Student Status: PASS")
+        else:
+            print("Student Status FAIL")
+
+students = input_students()
+students = calculet_average(students)
+display_results(students)
+                                       
